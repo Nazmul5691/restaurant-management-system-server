296b198a7bfc2918c4a33ac45597eaae727340ef
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-14 23:36:48 +0600

    update middleware

--- index.js
+++ index.js
@@ -81,18 +81,7 @@ async function run() {
             res.send(result);
         })
 
-        // app.patch('/users/admin/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) };
-        //     const updateDoc = {
-        //         $set: {
-        //             role: 'Admin'
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // })
-
+       
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
