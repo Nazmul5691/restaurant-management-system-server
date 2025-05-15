dba6117bd67bc49f901108f2fdf907581d955c9e
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-15 23:30:28 +0600

    admin check api create

--- index.js
+++ index.js
@@ -74,6 +74,11 @@ async function run() {
         })
 
 
+        app.get('/user/admin/:email', verifyToken, async(req, res) =>{
+            
+        })
+
+
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
